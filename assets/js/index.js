a7e50863c4ea95afe9ce769a6421de27692dd1c7
Kishor <117354246+Kishor-550@users.noreply.github.com>
2023-02-13 18:02:10 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 window.onload = localStorage.setItem("book_list", JSON.stringify(book_list));
 
 if (localStorage.getItem("isSignedIn") == "true") {
-	window.location.href += "./../../pages/user/homepage.html";
+	window.location.href = "./../../pages/user/homepage.html";
 }
 
 const usernameLogin = document.getElementById("username-sign-in");
@@ -27,9 +27,9 @@ signinForm.addEventListener("submit", function (e) {
 	if (isMatch === true) {
 		localStorage.setItem("isSignedIn", "true");
 		if (loginRole.value == "admin") {
-			window.location.href + "./../../pages/admin/admin_create-book.html";
+			window.location.href = "./../../pages/admin/admin_create-book.html";
 		} else {
-			window.location.href += "./../../pages/user/homepage.html";
+			window.location.href = "./../../pages/user/homepage.html";
 		}
 	} else {
 		alert("Oops! Log In failed Try Again");
