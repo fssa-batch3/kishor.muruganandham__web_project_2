c4653bf6ca4103aa58c1f4abb33600c8e840d72f
Kishor <kishor.muruganandham@fssa.freshworks.com>
2023-04-27 23:30:04 +0530

    Fixed Bugs

--- assets/js/pass.mjs
+++
@@ -1,9 +0,0 @@
-import { hash as _hash } from 'bcrypt';
-
-const password = 'mypassword';
-const saltRounds = 10;
-
-_hash(password, saltRounds, function(err, hash) {
-  // Store hash in your password DB.
-  console.log(hash);
-});
